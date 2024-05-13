d80611af08dbf27d87a6c9fab118b76ea498b219
Luise Muenchhausen <luisemuenchhausen@gmail.com>
2024-05-13 15:02:42 +0200

    Stupid Coaching 2.0

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    stop_talking = "I am going to work right now!"
+
+    # TODO: return coach answer to your_message
+    if @question == stop_talking || @question == stop_talking.upcase
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
